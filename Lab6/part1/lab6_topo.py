85da1083405da0d1d7a9a5c3ab6d979210117e92
sebs-rotors <mrbruhman3@gmail.com>
2024-12-13 13:33:48 -0800

    net start

--- Lab6/part1/lab6_topo.py
+++ Lab6/part1/lab6_topo.py
@@ -74,7 +74,7 @@ if __name__ == '__main__':
   #This part of the script is run when the script is executed
   topo = MyTopology() #Creates a topology
   c0 = RemoteController(name='c0', controller=RemoteController, ip='127.0.0.1', port=6633) #Creates a remote controller
-  net = Mininet(topo=topo, controller=c0, allocate_IPs=False) #Loads the topology
+  net = Mininet(topo=topo, controller=c0) #Loads the topology
   net.start() #Starts mininet
   CLI(net) #Opens a command line to run commands on the simulated topology
   net.stop() #Stops mininet
