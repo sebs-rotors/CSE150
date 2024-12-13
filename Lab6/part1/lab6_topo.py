3a61f7bc902fcc07fff2d2a39285e4cd184b852f
sebs-rotors <mrbruhman3@gmail.com>
2024-12-13 15:05:08 -0800

    return to working example

--- Lab6/part1/lab6_topo.py
+++ Lab6/part1/lab6_topo.py
@@ -7,63 +7,68 @@ from mininet.node import RemoteController
 class MyTopology(Topo):
   def __init__(self):
     Topo.__init__(self)
+   
+    # laptop1 = self.addHost('Laptop1', ip='200.20.2.8/24',defaultRoute="Laptop1-eth0")
+    coreSwitch = self.addSwitch('s1')
+    facultySwitch = self.addSwitch('s2')
+    studentSwitch = self.addSwitch('s3')
+    dataCenterSwitch = self.addSwitch('s4')
+    itSwitch = self.addSwitch('s5')
+    
+    # Core switch connections
+    self.addLink(coreSwitch, facultySwitch, port1=1, port2=1)
+    self.addLink(coreSwitch, studentSwitch, port1=2, port2=1)
+    self.addLink(coreSwitch, dataCenterSwitch, port1=3, port2=1)
+    self.addLink(coreSwitch, itSwitch, port1=4, port2=1)
+    
+    # We're gonna do hosts and links BY NETWORK:
+    # University Data Center
+    examServer = self.addHost('examServer', ip='10.100.100.2/24', mac='00:00:00:00:00:01', defaultRoute='examServer-eth1')
+    webServer = self.addHost('webServer', ip='10.100.100.20/24', mac='00:00:00:00:00:02', defaultRoute='webServer-eth1')
+    dnsServer = self.addHost('dnsServer', ip='10.100.100.56/24', mac='00:00:00:00:00:03', defaultRoute='dnsServer-eth1')
+    
+    self.addLink(examServer, dataCenterSwitch, port1=1, port2=2)
+    self.addLink(webServer, dataCenterSwitch, port1=1, port2=3)
+    self.addLink(dnsServer, dataCenterSwitch, port1=1, port2=4)
+    
+    # IT Department LAN
+    itWS = self.addHost('itWS', ip='10.40.3.30/24', mac='00:00:10:00:00:01', defaultRoute='itWS-eth1')
+    itPC = self.addHost('itPC', ip='10.40.3.254/24', mac='00:00:10:00:00:02', defaultRoute='itPC-eth1')
+    
+    self.addLink(itWS, itSwitch, port1=1, port2=2)
+    self.addLink(itPC, itSwitch, port1=1, port2=3)
+    
+    # Faculty LAN
+    facultyWS = self.addHost('facultyWS', ip='10.0.1.2/24', mac='00:00:20:00:00:01', defaultRoute='facultyWS-eth1')
+    facultyPC = self.addHost('facultyPC', ip='10.0.1.4/24', mac='00:00:20:00:00:02', defaultRoute='facultyPC-eth1')
+    printer = self.addHost('printer', ip='10.0.1.3/24', mac='00:00:20:00:00:03', defaultRoute='printer-eth1')
+    
+    self.addLink(facultyWS, facultySwitch, port1=1, port2=2)
+    self.addLink(facultyPC, facultySwitch, port1=1, port2=3)
+    self.addLink(printer, facultySwitch, port1=1, port2=4)
+    
+    # Student Housing LAN
+    studentPC1 = self.addHost('studentPC1', ip='10.0.2.2/24', mac='00:00:30:00:00:01', defaultRoute='studentPC1-eth1')
+    studentPC2 = self.addHost('studentPC2', ip='10.0.2.40/24', mac='00:00:30:00:00:02', defaultRoute='studentPC2-eth1')
+    labWS = self.addHost('labWS', ip='10.0.2.3/24', mac='00:00:30:00:00:03', defaultRoute='labWS-eth1')
+    
+    self.addLink(studentPC1, studentSwitch, port1=1, port2=2)
+    self.addLink(studentPC2, studentSwitch, port1=1, port2=3)
+    self.addLink(labWS, studentSwitch, port1=1, port2=4)
+    self.addLink(studentPC1, labWS, port1=2, port2=2)
+    
+    # Internet
+    trustedPC = self.addHost('trustedPC', ip='10.0.203.6/32', mac='00:00:40:00:00:01', defaultRoute='trustedPC-eth1')
+    guest1 = self.addHost('guest1', ip='10.0.196.6/32', mac='00:00:40:00:00:02', defaultRoute='guest1-eth1')
+    guest2 = self.addHost('guest2', ip='10.0.198.10/32', mac='00:00:40:00:00:03', defaultRoute='guest2-eth1')
 
-    # laptop1 = self.addHost('Laptop1', ip='200.20.2.8/24',defaultRoute="Laptop1-eth1")
+    self.addLink(guest1, coreSwitch, port1=1, port2=5)
+    self.addLink(guest2, coreSwitch, port1=1, port2=6)
+    self.addLink(trustedPC, coreSwitch, port1=1, port2=7)
 
     # switch1 = self.addSwitch('s1')
 
     # self.addLink(laptop1, switch1, port1=1, port2=2)
-    #Switches and links
-    coreSwitch = self.addSwitch("s1")
-    facultySwitch = self.addSwitch("s2")
-    studentSwitch = self.addSwitch("s3")
-    itSwitch = self.addSwitch("s4")
-    dataCenterSwitch = self.addSwitch("s5")
-    self.addLink(facultySwitch,coreSwitch,port1=1,port2=1)
-    self.addLink(studentSwitch,coreSwitch,port1=1,port2=2)
-    self.addLink(itSwitch,coreSwitch,port1=1,port2=3)
-    self.addLink(dataCenterSwitch,coreSwitch,port1=1,port2=4)
-
-    #Faculty LAN
-    facultyWS = self.addHost('facultyWS', mac="00:00:00:00:00:01",ip='10.0.1.2/24',defaultRoute="facultyWS-eth1")
-    printer = self.addHost('printer', mac="00:00:00:00:00:02",ip='10.0.1.3/24',defaultRoute="printer-eth1")
-    facultyPC = self.addHost('facultyPC', mac="00:00:00:00:00:03",ip='10.0.1.4/24',defaultRoute="facultyPC-eth1")
-    self.addLink(facultyWS,facultySwitch,port1=1,port2=2)
-    self.addLink(printer,facultySwitch,port1=1,port2=3)
-    self.addLink(facultyPC,facultySwitch,port1=1,port2=4)
-
-    #Student Housing LAN
-    studentPC1 = self.addHost('studentPC1', mac="00:00:00:00:00:04",ip='10.0.2.2/24',defaultRoute="studentPC1-eth1")
-    labWS = self.addHost('labWS', mac="00:00:00:00:00:05",ip='10.0.2.3/24',defaultRoute="labWS-eth1")
-    studentPC2 = self.addHost('studentPC2', mac="00:00:00:00:00:06",ip='10.0.2.40',defaultRoute="studentPC2-eth1")
-    self.addLink(studentPC1,studentSwitch,port1=1,port2=2)
-    self.addLink(labWS,studentSwitch,port1=1,port2=3)
-    self.addLink(studentPC2,studentSwitch,port1=1,port2=4)
-
-    #IT Department LAN
-    itWS = self.addHost('itWS', mac="00:00:00:00:00:07",ip='10.40.3.30',defaultRoute="itWS-eth1")
-    itPC = self.addHost('itPC', mac="00:00:00:00:00:08",ip='10.40.3.254',defaultRoute="itPC-eth1")
-    self.addLink(itWS,itSwitch,port1=1,port2=2)
-    self.addLink(itPC,itSwitch,port1=1,port2=3)
-
-    #University Data Center
-    facultyExamServer = self.addHost('examServer', mac="00:00:00:00:00:09",ip='10.100.100.2/24',defaultRoute="examServer-eth1")
-    webServer = self.addHost('webServer', mac="00:00:00:00:00:10",ip='10.100.100.20',defaultRoute="webServer-eth1")
-    dnsServer = self.addHost('dnsServer', mac="00:00:00:00:00:11",ip='10.100.100.56',defaultRoute="dnsServer-eth1")
-    self.addLink(facultyExamServer,dataCenterSwitch,port1=1,port2=2)
-    self.addLink(webServer,dataCenterSwitch,port1=1,port2=3)
-    self.addLink(dnsServer,dataCenterSwitch,port1=1,port2=4)
-
-    #Internet
-    trustedPC = self.addHost('trustedPC', mac="00:00:00:00:00:12",ip='10.0.203.6/32',defaultRoute="trustedPC-eth1")
-    guest1 = self.addHost('guest1', mac="00:00:00:00:00:13",ip='10.0.198.6/32',defaultRoute="guest1-eth1")
-    guest2 = self.addHost('guest2', mac="00:00:00:00:00:14",ip='10.0.198.10/32',defaultRoute="guest2-eth1")
-    self.addLink(trustedPC,coreSwitch,port1=1,port2=5)
-    self.addLink(guest1,coreSwitch,port1=1,port2=6)
-    self.addLink(guest2,coreSwitch,port1=1,port2=7)
-
-
-
 
 if __name__ == '__main__':
   #This part of the script is run when the script is executed
@@ -73,4 +78,3 @@ if __name__ == '__main__':
   net.start() #Starts mininet
   CLI(net) #Opens a command line to run commands on the simulated topology
   net.stop() #Stops mininet
-
